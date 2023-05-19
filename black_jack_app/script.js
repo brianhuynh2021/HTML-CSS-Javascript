cde5a0ec16fdad48801da318918b8d58537f9ebb
Brian Huynh <huynh2102@gmail.com>
2023-05-20 00:22:03 +0700

    update codes with lucky_number

---
+++ black_jack_app/script.js
@@ -0,0 +1,32 @@
+// let firstName = "Brian";
+// let lastName = "Huynh"
+
+// let fullName = firstName + " " + lastName;
+
+// let name = "Linda"
+// let greeting = "Hi there"
+// function greet() {
+//     console.log(greeting + ", " + name + "!")
+// }
+// greet()
+// console.log(fullName)
+
+// let myPoints = 3;
+
+// function add3Points() {
+//     myPoints += 3;
+// }
+
+// function remove1Point() {
+//     myPoints -= 1;
+// }
+
+// add3Points()
+
+
+console.log("2" + 2);
+console.log(11+7);
+console.log(6+"5");
+console.log("My points: " + 5 + 9);
+console.log(2 + 2);
+console.log("11" + "14");
