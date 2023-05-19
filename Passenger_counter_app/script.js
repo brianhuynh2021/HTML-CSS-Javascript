aedf2ab4eddf44e92c3f7fef1e667d0ccbc4fedf
Brian Huynh <huynh2102@gmail.com>
2023-05-19 16:54:08 +0700

    update more feature total entries

--- Passenger_counter_app/script.js
+++ Passenger_counter_app/script.js
@@ -1,16 +1,22 @@
 let count = 0
+let totalEntries = 0;
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("pre-entry");
+let totalEl = document.getElementById("total-el");
 
 function increment() {
     count += 1;
-    
     countEl.textContent = count;
 }
 
 function save(){
+    totalEntries += count;
     let countStr = count + " - ";
-    saveEl.textContent += countStr
+    saveEl.textContent += countStr;
     count = 0;
     countEl.innerText = 0;
+    totalEl.textContent = totalEntries;
 }
+
+
+
